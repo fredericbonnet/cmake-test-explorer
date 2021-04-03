f1cd8d213a4c66b288387124fbaf0fe4d7f73bc6
HO-COOH <szpeter@yeah.net>
2021-04-03 09:51:39 -0500

    fix await to activate

--- src/main.ts
+++ src/main.ts
@@ -34,15 +34,13 @@ export async function activate(context: vscode.ExtensionContext) {
     );
     if (!cmakeExtension) {
       const message = `CMake integration is enabled but the CMake Tools extension is not installed`;
-      log.warn(
-        message
-      );
+      log.warn(message);
       vscode.window.showErrorMessage(message);
     } else if (!cmakeExtension.isActive) {
       log.warn(
         `CMake integration is enabled but the CMake Tools extension is not active`
       );
-      cmakeExtension.activate();
+      await cmakeExtension.activate();
     }
   }
 
