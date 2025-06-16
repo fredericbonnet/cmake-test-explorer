db75e29ec47245c5cabf6d7326616bb624ceb8d5
Frédéric Bonnet <fredericbonnet@free.fr>
2025-06-16 23:21:00 +0200

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,8 @@ import { createCmakeController } from './cmake-controller';
 
 /**
  * Main extension entry point
- *
- * Code is from the vscode-example-test-adapter extension template
  */
 export async function activate(context: vscode.ExtensionContext) {
 	const controller = createCmakeController();
 	context.subscriptions.push(controller);
 }
-
-export function deactivate() {}
